d72765dec5660dd3355ed0e0f44a6cd1a23c0c3f
Vithushan <sutharshanvithushan@gmail.com>
2024-12-11 05:04:22 +0530

    feat: dark mode and serif font toggle

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {},
     fontFamily: {
-      serif: ["ui-serif", "Georgia", "serif"],
+      serif: ["IBM Plex Serif", "ui-serif", "Georgia", "serif"],
       sans: ["Fira Sans", "sans"],
       mono: ["Fira Mono", "ui-monospace", "SFMono-Regular"],
     },
